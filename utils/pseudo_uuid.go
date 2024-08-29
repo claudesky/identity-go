0ab28cacb106575fa823952e6f929a4fcf8ffba4
Claude Kirke <claudekirke@gmail.com>
2024-08-30 01:08:33 +0800

    feat: lowercase uuids

--- utils/pseudo_uuid.go
+++ utils/pseudo_uuid.go
@@ -3,6 +3,7 @@ package utils
 import (
 	"crypto/rand"
 	"fmt"
+	"strings"
 )
 
 // Note - NOT RFC4122 compliant
@@ -15,7 +16,9 @@ func PseudoUUID() (uuid string) {
 		return
 	}
 
-	uuid = fmt.Sprintf("%X-%X-%X-%X-%X", b[0:4], b[4:6], b[6:8], b[8:10], b[10:])
+	uuid = strings.ToLower(
+		fmt.Sprintf("%X-%X-%X-%X-%X", b[0:4], b[4:6], b[6:8], b[8:10], b[10:]),
+	)
 
 	return
 }
