8c4e0f0bd59de5b6cfb2db86cadf0205d835f9ca
Claude Kirke <claudekirke@gmail.com>
2024-08-30 03:46:23 +0800

    chore: add sample data

---
+++ sample-data/init.sql
@@ -0,0 +1,30 @@
+create table users (
+    id uuid primary key,
+    password text,
+    name text constraint name_length check (char_length(name) <= 255),
+    email text constraint email_length check (char_length(email) <= 255),
+    email_verified_on timestamp,
+    phone_number text constraint phone_number_length check (char_length(phone_number) <= 255),
+    phone_number_verified_on timestamp
+);
+
+create table audiences (
+    id uuid primary key,
+    name text
+);
+
+create table token_families (
+    id uuid,
+    sub uuid,
+    last_issued uuid,
+    created_at timestamp,
+    last_issued_at timestamp
+);
+
+insert into users (id, name, email, password)
+values (
+    '0615b123-1a98-405b-bc44-6d41ad6a193c',
+    'admin',
+    'admin@example.org',
+    '$2a$12$xrjwIS2d.hptiD/CEKKqxO5kVYjuWcWwxTNeXDT2bQJRlJweKGLu.'
+)
