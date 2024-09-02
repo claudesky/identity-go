e4f7d7364b0640e178f97e56598df925e24a2b4b
Claude Kirke <claudekirke@gmail.com>
2024-09-03 04:16:28 +0800

    feat: token_family model

---
+++ models/token_family.go
@@ -0,0 +1,11 @@
+package models
+
+import "time"
+
+type TokenFamily struct {
+	Id           string    `json:"id"`
+	Sub          string    `json:"sub"`
+	LastIssued   string    `json:"last_issued"`
+	CreatedAt    time.Time `json:"created_at"`
+	LastIssuedAt time.Time `json:"last_issued_at"`
+}
