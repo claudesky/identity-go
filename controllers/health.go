4627b2e4d0d35b71c5c763df150cef6ff66d9207
Claude Kirke <claudekirke@gmail.com>
2025-02-04 00:08:55 +0900

    chore: remove redundant health check

--- controllers/health.go
+++ controllers/health.go
@@ -18,21 +18,12 @@ func NewHealthController() *HealthController {
 }
 
 func (c *HealthController) RegisterRoutes(mux *http.ServeMux) {
-	mux.HandleFunc("GET /health/hello-word", c.helloWorld)
 	mux.HandleFunc("GET /health/check", c.check)
 }
 
 func (c *HealthController) check(w http.ResponseWriter, _ *http.Request) {
 	if c.healthy {
 		json.NewEncoder(w).Encode(&Message{Message: "ok"})
-	} else {
-		http.Error(w, "Service Unavailable", http.StatusServiceUnavailable)
-	}
-}
-
-func (c *HealthController) helloWorld(w http.ResponseWriter, _ *http.Request) {
-	if c.healthy {
-		json.NewEncoder(w).Encode(&Message{Message: "Hello, world!"})
 	} else {
 		http.Error(w, "Service Unavailable", http.StatusServiceUnavailable)
 	}
