2429e3aea077df33468388a95f446765e244a64b
Claude Kirke <claudekirke@gmail.com>
2025-02-14 20:04:33 +0900

    feat: add register request model and repository

---
+++ models/register_request.go
@@ -0,0 +1,10 @@
+package models
+
+import "time"
+
+type RegisterRequest struct {
+	Id        string     `json:"id"`
+	Password  *string    `json:"-"`
+	Email     *string    `json:"email"`
+	CreatedAt *time.Time `json:"created_at"`
+}
---
+++ repositories/register_request.go
@@ -0,0 +1,63 @@
+package repositories
+
+import (
+	"context"
+
+	"github.com/claudesky/identity-go/models"
+	"github.com/claudesky/identity-go/services"
+	"github.com/jackc/pgx/v5"
+)
+
+type RegisterRequestRepository struct {
+	db *services.Database
+}
+
+func NewRegisterRequestRepository(
+	db *services.Database,
+) *RegisterRequestRepository {
+	return &RegisterRequestRepository{db}
+}
+
+func (r *RegisterRequestRepository) GetRegisterRequestById(
+	ctx context.Context,
+	id string,
+) (
+	*models.RegisterRequest,
+	error,
+) {
+	query := `select * from register_requests where id = @id`
+	args := pgx.NamedArgs{"id": id}
+
+	rows, err := r.db.Query(ctx, query, args)
+	if err != nil {
+		return nil, err
+	}
+
+	result, err := pgx.CollectOneRow(
+		rows,
+		pgx.RowToStructByNameLax[models.RegisterRequest],
+	)
+	return &result, err
+}
+
+func (r *RegisterRequestRepository) GetRegisterRequestByEmail(
+	ctx context.Context,
+	email string,
+) (
+	*models.RegisterRequest,
+	error,
+) {
+	query := `select * from register_requests where email = @email`
+	args := pgx.NamedArgs{"email": email}
+
+	rows, err := r.db.Query(ctx, query, args)
+	if err != nil {
+		return nil, err
+	}
+
+	result, err := pgx.CollectOneRow(
+		rows,
+		pgx.RowToStructByNameLax[models.RegisterRequest],
+	)
+	return &result, err
+}
