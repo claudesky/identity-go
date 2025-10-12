d1eb88eadca17c70f0bb98b1aa2775f4092c8047
Claude Kirke <claudekirke@gmail.com>
2025-10-13 07:15:54 +0900

    chore: debug configuration

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,7 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true },
+  devtools: { enabled: process.env.NODE_ENV !== 'production' },
+  debug: process.env.NODE_ENV !== 'production',
 
   devServer: {
     port: 3000
